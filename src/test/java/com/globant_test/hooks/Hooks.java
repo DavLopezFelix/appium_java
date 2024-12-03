c58931c465e941fe459c24704d3bb62bf9f066db
David Lop <dlopezf.19@gmail.com>
2024-12-03 11:55:58 -0500

    support screenshot

--- src/test/java/com/globant_test/hooks/Hooks.java
+++ src/test/java/com/globant_test/hooks/Hooks.java
@@ -3,6 +3,7 @@ package com.globant_test.hooks;
 import com.globant_test.config.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -15,15 +16,16 @@ public class Hooks {
         DriverFactory.getDriver();
     }
 
-    @After
+    @After(order = 2)
     public static void takeScreenshot(Scenario scenario){
         if (scenario.isFailed()){
+            System.out.println("Smile...taking a picture");
             TakesScreenshot takesScreenShot = (TakesScreenshot) DriverFactory.getDriver();
             byte [] screenshot = takesScreenShot.getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png", screenshot.toString());
         }
     }
-    @After
+    @After(order = 1)
     public void tearDown(){
         DriverFactory.quitDriver();
         System.out.println("Aplicacion cerrada!");
